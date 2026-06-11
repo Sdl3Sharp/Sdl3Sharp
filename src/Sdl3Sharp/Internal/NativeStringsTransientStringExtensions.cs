f00d8b8e36f5d5ae87dcda3e0d5ea5e5fc2be503
Felix Rüdiger <fruediger.90@googlemail.com>
2026-06-11 15:53:01 +0200

    I forgot to add a convenience extension method to convert TransientString<char> to managed strings
    It's important to add that now, because I don't want dependent projects (e.g., Sdl3Sharp.Ttf) to make up their own solution to this (and we end up with multiple instances of the same code distribute across multiple projects), but rather rely on a centralized implementation.

---
+++ src/Sdl3Sharp/Internal/NativeStringsTransientStringExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Sdl3Sharp.Internal;
+
+internal static class NativeStringsTransientStringExtensions
+{
+	extension(NativeStrings.TransientString<char> utf16)
+	{
+		[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+		internal string? ToManaged()
+		{
+			unsafe
+			{
+				if (utf16.Buffer is null)
+				{
+					return null;
+				}
+
+				if (utf16.Length is 0)
+				{
+					return string.Empty;
+				}
+
+				return new string(utf16.Buffer, 0, unchecked((int)nuint.Max(utf16.Length, int.MaxValue)));
+			}
+		}
+	}
+}
